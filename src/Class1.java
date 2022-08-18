400e396f37d82f5f97c591a54cea3511beeb638c
Neha14abc <kusum14032000@gmail.com>
2022-08-18 10:45:14 +0530

    Java Day 2c

---
+++ src/Class1.java
@@ -0,0 +1,10 @@
+public class Class1 {
+    public static void main(String[]args) {
+        System.out.println(" hello 7a");
+        System.out.println("hello 7b");
+        System.out.println("hello 7c");
+        System.out.println("hello 7d");
+        System.out.println("hello 7e");
+
+    }
+}
---
+++ src/Class2.java
@@ -0,0 +1,10 @@
+public class Class2 {
+    public static void main(String[]args) {
+        System.out.println("hello 8a");
+        System.out.println("hello 8b");
+        System.out.println("hello 8c");
+        System.out.println("hello 8d");
+        System.out.println("hello 8e");
+
+    }
+}
